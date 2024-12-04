cfadd6cfdb23352473dcda012e8cccf45e73d1a2
Marcel Kwaschny <9392778+marcelkwaschny@users.noreply.github.com>
2024-12-04 16:44:08 +0100

    ✨ Added an implementation of the fortinet netwitness pipeline

--- sigma/pipelines/netwitness/__init__.py
+++ sigma/pipelines/netwitness/__init__.py
@@ -1,7 +1,9 @@
 """Mappings of identifier to pipelines used by pySigma"""
 
-from .windows import netwitness_windows_pipeline
+from .fortinet import netwitness_fortinet_pipeline  # pylint:disable=import-error
+from .windows import netwitness_windows_pipeline  # pylint:disable=import-error
 
 pipelines = {
     "netwitness_windows": netwitness_windows_pipeline,
+    "netwitness_fortinet": netwitness_fortinet_pipeline,
 }
