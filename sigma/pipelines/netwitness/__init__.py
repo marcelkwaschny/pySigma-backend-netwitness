7633c7fecacc7ef09fe4f3781444b8d3ae37954c
Marcel Kwaschny <9392778+marcelkwaschny@users.noreply.github.com>
2024-12-11 13:19:46 +0100

    🔥 Removed fortinet pipeline as it is to specific, Updated all dev dependencies to latest versions

--- sigma/pipelines/netwitness/__init__.py
+++ sigma/pipelines/netwitness/__init__.py
@@ -1,9 +1,7 @@
 """Mappings of identifier to pipelines used by pySigma"""
 
-from .fortinet import netwitness_fortinet_pipeline  # pylint:disable=import-error
 from .windows import netwitness_windows_pipeline  # pylint:disable=import-error
 
 pipelines = {
     "netwitness_windows": netwitness_windows_pipeline,
-    "netwitness_fortinet": netwitness_fortinet_pipeline,
 }
