9d5fbeec7d2293522fa4766ca3e7bcfa4fd78ae3
Marcel Kwaschny <9392778+marcelkwaschny@users.noreply.github.com>
2025-02-26 15:38:57 +0100

    🐛 Fixed a bug using the combination of string conversion and windash
    Using sigma expansions in combination with string conversions results in
    a sigma expansion that wrapped into a string. This string is then
    plain converted into a query. Therefore the expansion is not unwrapped.
    A custom transformation was added to fix this issue.

--- sigma/pipelines/netwitness/__init__.py
+++ sigma/pipelines/netwitness/__init__.py
@@ -1,6 +1,6 @@
 """Mappings of identifier to pipelines used by pySigma"""
 
-from .windows import netwitness_windows_pipeline  # pylint:disable=import-error
+from .windows import netwitness_windows_pipeline
 
 pipelines = {
     "netwitness_windows": netwitness_windows_pipeline,
